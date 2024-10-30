9b0ad93300d47326d6e596d6c834bc9be078944b
griphax <migueldlr88@gmail.com>
2024-10-30 15:49:15 -0500

    First Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,31 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+function countdownTimer() {
+    const endDate = new Date("October 30, 2024 19:59:59").getTime();
+    const timer = setInterval(function() {
+        const now = new Date().getTime();
+        const distance = endDate - now;
+
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerText = days;
+        document.getElementById("hours").innerText = hours;
+        document.getElementById("minutes").innerText = minutes;
+        document.getElementById("seconds").innerText = seconds;
+
+        if (distance < 0) {
+            clearInterval(timer);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+}
+
+countdownTimer();
+
 (function($) {
 
 	var	$window = $(window),
